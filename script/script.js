6fc5a5637c7fdf9aa7de9c2d004b0cd70c88da11
Maaria <Maaria@Maarias-MacBook-Air.local>
2015-11-30 15:56:16 -0500

    sigh

--- script/script.js
+++ script/script.js
@@ -20,18 +20,16 @@ var popByState = d3.map();
 
 //Scales
 var scaleR = d3.scale.sqrt().range([5,130]),
-    scaleColor = d3.scale.linear().domain([70,90]).range(['white','red']);
+    scaleColor = d3.scale.linear().domain([70,90]).range(['white','purple']);
 
 //import data
 queue()
 	.defer(d3.json, "data/gz_2010_us_040_00_5m.json")
     .defer(d3.csv, "data/2014_state_pop.csv", parseData)
 	.await(function(err, states, pop){
-
-        //mine data to complete scales
-        var maxPop = d3.max(pop);
+	//mine data to complete scales
+	var maxPop = d3.max(pop);
         scaleR.domain([0,maxPop]);
-
         //construct a new array of data
         var data = states.features.map(function(d){
             var centroid = path.centroid(d); //provides two numbers [x,y] indicating the screen coordinates of the state
@@ -47,19 +45,11 @@ queue()
         });
         console.log(data);
 
-		var nodes = map.selectAll('.state')
-            .data(data, function(d){return d.state});
-
-        //Represent as a cartogram of populations
-        var nodesEnter = nodes.enter()
+        var nodes = map.selectAll('.state')
+            .data(data, function(d){return d.state})
+            .enter()
             .append('g')
             .attr('class','state');
-        nodes.exit().remove();
-
-        nodes
-            .attr('transform',function(d){
-                return 'translate('+d.x+','+d.y+')';
-            })
         nodes
             .append('circle')
             .attr('r',function(d){
@@ -73,17 +63,74 @@ queue()
             .style('fill-opacity',.7);
         nodes
             .append('text')
+            .attr('class','names')
             .text(function(d){
-                return d.name;
+                return d.fullName;
             })
             .attr('text-anchor','middle');
 
         //TODO: create a force layout
         //with what physical parameters?
         var force = d3.layout.force()
-        //on "tick" event ...
+            .size([width,height])
+            .charge(-85)
+            .gravity(.01);
+            //.friction(0.6);
 
-	});
+        force.nodes(data)
+            .on('tick', onForceTick)
+            .start();
+
+        function onForceTick(e){
+            var q = d3.geom.quadtree(data),
+                i = 0,
+                n = data.length;
+
+            while( ++i<n ){
+                q.visit(collide(data[i]));
+            }
+
+            nodes
+                .each(gravity(e.alpha*.1))
+                .each(collide(e.alpha*.1))
+                .attr('transform',function(d){
+                    return 'translate('+d.x+','+d.y+')';
+                })
+
+            //custom gravity: data points gravitate towards a straight line
+            function gravity(k){
+                return function(d){
+                    d.y += (d.y0 - d.y)*k;
+                    d.x += (d.x0 - d.x)*k;
+                }
+            }
+
+            function collide(dataPoint){
+                var nr = dataPoint.r + 5,
+                    nx1 = dataPoint.x - nr,
+                    ny1 = dataPoint.y - nr,
+                    nx2 = dataPoint.x + nr,
+                    ny2 = dataPoint.y + nr;
+
+                return function(quadPoint,x1,y1,x2,y2){
+                    if(quadPoint.point && (quadPoint.point !== dataPoint)){
+                        var x = dataPoint.x - quadPoint.point.x,
+                            y = dataPoint.y - quadPoint.point.y,
+                            l = Math.sqrt(x*x+y*y),
+                            r = nr + quadPoint.point.r;
+                        if(l<r){
+                            l = (l-r)/l*.1;
+                            dataPoint.x -= x*= (l*.05);
+                            dataPoint.y -= y*= l;
+                            quadPoint.point.x += (x*.05);
+                            quadPoint.point.y += y;
+                        }
+                    }
+                    return x1>nx2 || x2<nx1 || y1>ny2 || y2<ny1;
+                }
+            }
+        }
+    });
 
 function parseData(d){
     //Use the parse function to populate the lookup table of states and their populations/% pop 18+
